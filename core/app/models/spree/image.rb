b5e162b57969b748ad03f577c67e5d8e22737049
Jamie Stephens <jamie@blueroot.com>
2014-01-08 16:20:18 -0600

    changing the order of the validations for paperclip to not crap out

--- core/app/models/spree/image.rb
+++ core/app/models/spree/image.rb
@@ -1,7 +1,6 @@
 module Spree
   class Image < Asset
-    validates_attachment_presence :attachment
-    validate :no_attachment_errors
+    
 
     has_attached_file :attachment,
                       styles: { mini: '48x48>', small: '100x100>', product: '240x240>', large: '600x600>' },
@@ -10,6 +9,8 @@ module Spree
                       path: ':rails_root/public/spree/products/:id/:style/:basename.:extension',
                       convert_options: { all: '-strip -auto-orient -colorspace sRGB' }
 
+    validates_attachment_presence :attachment
+    validate :no_attachment_errors
     # save the w,h of the original image (from which others can be calculated)
     # we need to look at the write-queue for images which have not been saved yet
     after_post_process :find_dimensions
